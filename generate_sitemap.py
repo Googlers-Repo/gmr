dc66d2cb746da91d9d4f505ca3cf3a03b155bf8a
Der_Googler <54764558+DerGoogler@users.noreply.github.com>
2024-06-25 12:58:04 +0200

    Use https instead of http

--- generate_sitemap.py
+++ generate_sitemap.py
@@ -14,7 +14,7 @@ modules = data.get("modules", [])
 urlset = ET.Element("urlset", xmlns="http://www.sitemaps.org/schemas/sitemap/0.9", **{"xmlns:image": "http://www.google.com/schemas/sitemap-image/1.1"})
 
 # Base URL for the modules
-base_url = "http://mmrl.dergoogler.com/?module="
+base_url = "https://mmrl.dergoogler.com/?module="
 
 # Add URLs to the sitemap
 for module in modules:
